7926f169fc1b7255cd34cea1c2175ccc281e5997
cjpm1983 en la nube <cjpm1983@gmail.com>
2020-06-16 17:54:55 +0000

    primerodesdepythonanywhere

---
+++ apk.py
@@ -0,0 +1,61 @@
+from flask import Flask, request
+import telegram
+import telepot
+import urllib3
+from telebot.credentials import bot_token, bot_user_name,URL
+from telebot.mastermind import get_response
+
+
+# You can leave this bit out if you're using a paid PythonAnywhere account
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+# end of the stuff that's only needed for free accounts
+
+
+global bot
+global TOKEN
+TOKEN = bot_token
+#bot = telegram.Bot(token=TOKEN)
+bot = telepot.Bot(token=TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+
+    response = get_response(text)
+    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+
+    return 'ok'
+
+@app.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+@app.route('/')
+def index():
+    return '.'
+
+@app.route('/check')
+def check():
+    return 'Corriendo al kilo'
+
+
+#if __name__ == '__main__':
+#    app.run(threaded=True)
